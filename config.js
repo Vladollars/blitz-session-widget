56fe43d410120500457c8087f90a61ea850f2166
Vladollars <vladollars@gmail.com>
2026-08-25 09:21:56 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,6 +1,1 @@
-/*
-  После deploy Cloudflare Worker вставь сюда его адрес.
-  Пример:
-  window.BLITZ_SESSION_API = "https://blitz-session-api.vladollars.workers.dev";
-*/
-window.BLITZ_SESSION_API = "https://blitz-session-api.vladollars-blitz.workers.dev";
+window.BLITZ_SESSION_API="https://blitz-session-api.vladollars-blitz.workers.dev";
